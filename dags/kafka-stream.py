31f72cd1f11a169b4ed4d6e77f1ce7dffc2268a5
tibarkeb <berassut@gmail.com>
2023-12-26 09:45:41 +0300

    configure kafka to add data to control center

--- dags/kafka-stream.py
+++ dags/kafka-stream.py
@@ -41,10 +41,16 @@ def format_data(res):
 
 def stream_data():
     import json
+    from kafka import KafkaProducer
+    import time
+
     res = get_data()
     res = format_data(res)
-    print(json.dumps(res, indent=3))
-   
+    # print(json.dumps(res, indent=3))
+
+    producer = KafkaProducer(bootstrap_servers=['localhost:9092'], max_block_ms=5000)
+
+    producer.send('users_created', json.dumps(res).encode('utf-8'))
 
 with DAG ('user_automation', 
           default_args = default_args, 
